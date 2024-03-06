1a0c05bc11b5c4188d75d190c9c328e8df48b98f
Serhii Haiduchyk <jaegarn141@gmail.com>
2024-03-06 12:59:25 +0200

    features: added index function.

--- core/views.py
+++ core/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from .models import Position, Worker, Task, TaskType
+
+
+@login_required
+def index(request: HttpRequest) -> HttpResponse:
+    """View function for the home page of the site."""
+
+    num_workers = Worker.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_workers": num_workers,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "core/index.html", context=context)

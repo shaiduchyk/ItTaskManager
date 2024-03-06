34faa5746449dc7250121b802f8823620e55b26d
Serhii Haiduchyk <jaegarn141@gmail.com>
2024-03-06 17:57:10 +0200

    features: added my-tasks page with parameters.

--- core/views.py
+++ core/views.py
@@ -5,21 +5,48 @@ from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .models import Position, Worker, Task, TaskType
+
+import pretty_errors
+
+from .models import Position, Worker, Task, TaskType, Project
 
 
 @login_required
 def index(request: HttpRequest) -> HttpResponse:
-    """View function for the home page of the site."""
-
-    num_workers = Worker.objects.count()
-
-    num_visits = request.session.get("num_visits", 0)
-    request.session["num_visits"] = num_visits + 1
-
+    num_tasks = Task.objects.count()
+    last_added_task = Task.objects.order_by("-created_at").first()
+    completed_tasks = Project.objects.filter(is_completed=True).count()
     context = {
-        "num_workers": num_workers,
-        "num_visits": num_visits + 1,
+        "num_tasks": num_tasks,
+        "last_added_task": last_added_task,
+        "completed_tasks": completed_tasks,
     }
 
-    return render(request, "core/index.html", context=context)
+    return render(request, "core/index.html", context)
+
+
+class MyTasksListView(generic.ListView):
+    model = Task
+    template_name = 'core/my_tasks.html'
+    context_object_name = 'task_list'
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.incomplete_tasks_count = None
+        self.completed_tasks_count = None
+
+    def get_queryset(self):
+        completed_tasks_count = Task.objects.filter(assignees=self.request.user, is_completed=True).count()
+        incomplete_tasks_count = Task.objects.filter(assignees=self.request.user, is_completed=False).count()
+
+        self.completed_tasks_count = completed_tasks_count
+        self.incomplete_tasks_count = incomplete_tasks_count
+
+        return Task.objects.filter(assignees=self.request.user)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context["completed_tasks_count"] = self.completed_tasks_count
+        context["incomplete_tasks_count"] = self.incomplete_tasks_count
+        return context

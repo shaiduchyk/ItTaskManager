d30afd2af42eb5baa371cd383f9f045fcadcea12
Serhii Haiduchyk <jaegarn141@gmail.com>
2024-03-06 12:58:53 +0200

    refactor: added class Project, and refactor class Meta, __str__ methods.

--- core/models.py
+++ core/models.py
@@ -1,30 +1,36 @@
 from django.conf import settings
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.db.models import UniqueConstraint
 
 
 class Position(models.Model):
-    name = models.CharField(max_length=25)
+    name = models.CharField(max_length=63)
 
     def __str__(self):
         return self.name
 
-    class Meta:
-        verbose_name_plural = "Position"
-
 
 class Worker(AbstractUser):
-    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.CASCADE,
+        default=None,
+    )
 
     class Meta:
-        ordering = ["first_name", "last_name", "position"]
+        verbose_name_plural = "Workers"
+        constraints = [
+            UniqueConstraint(fields=["username"], name="unique_username")
+        ]
+        ordering = ["last_name", "first_name", "position"]
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} position: {self.position}"
 
 
 class TaskType(models.Model):
-    type = models.CharField(max_length=255, unique=True)
-
-    def __str__(self):
-        return self.type
+    type = models.CharField(max_length=63)
 
 
 class Task(models.Model):
@@ -42,14 +48,36 @@ class Task(models.Model):
         (PRIORITY_CRITICAL, "Critical"),
     ]
 
-    name = models.CharField(max_length=255, unique=True)
+    task_name = models.CharField(max_length=255)
     description = models.TextField()
     deadline = models.DateField()
     is_completed = models.BooleanField(default=False)
-    priority = models.CharField(max_length=255, choices=PRIORITY_CHOICES, default="Low")
-    task_type = models.ForeignKey("TaskType", on_delete=models.CASCADE)
-    assignees = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="task")
-    created_at = models.DateTimeField(auto_now_add=True)
+    priority = models.CharField(max_length=23, choices=PRIORITY_CHOICES, default="low")
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE, related_name="tasks")
+    assignees = models.ManyToManyField(
+        to=settings.AUTH_USER_MODEL,
+        related_name="tasks"
+    )
+    created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.name} assigned to {self.assignees} with {self.priority} priority"
+        assignee_names = ", ".join(str(assignee) for assignee in self.assignees.all())
+        return (f"{self.task_name} assigned to {assignee_names}"
+                f" with {self.priority} priority")
+
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=120)
+    description = models.TextField()
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now=True)
+    assignees = models.ManyToManyField(
+        to=settings.AUTH_USER_MODEL,
+        related_name="project"
+    )
+    task_list = models.ManyToManyField(Task, related_name="project")
+
+    def get_task_status_count(self):
+        completed_tasks = self.task_list.filter(is_completed=True).count()
+        incomplete_tasks = self.task_list.filter(is_completed=False).count()
+        return {"completed_tasks": completed_tasks, "incomplete_tasks": incomplete_tasks}

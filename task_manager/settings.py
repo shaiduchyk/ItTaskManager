6ce643ab46bace335c8f8b98c6647e09a771766d
Serhii Haiduchyk <jaegarn141@gmail.com>
2024-03-18 15:43:54 +0200

    refactor: remove DJANGO_SECRET_KEY for deploy.

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-y@&=5$7c#j3^qsj0u1!3=*rx5$+ni2n-ky=&q=9o&c6c49k5(3")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
